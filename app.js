37a83ed9b967fb326f10b7d2512e038462650ce1
MaryamJami1 <maryamchouhan373@gmail.com>
2024-02-01 21:19:38 +0500

    java practice

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+let name = "maryam";
+
+console.log(name);
+
+let age=25
+
+console.log(age);
+
+let isFolow = true;
+
+console.log(isFolow)
+
+const product = {
+title: "ball pen"
+}
+console.log(product)
+
